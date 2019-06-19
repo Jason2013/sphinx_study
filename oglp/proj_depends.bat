58ee988430c2fb2d804805ca38418fcdf466a922
Jason Chen <shchenchang@gmail.com>
2019-06-19 17:40:15 +0800

    Update `proj_depends.svg` image.

--- oglp/proj_depends.bat
+++ oglp/proj_depends.bat
@@ -1,2 +1,3 @@
-SET DOT_APP=C:\Program Files (x86)\Graphviz2.38\bin\dot.exe
-%DOT_APP% 
+SET DOT_GV=proj_depends
+SET DOT_APP="C:\Program Files (x86)\Graphviz2.38\bin\dot.exe"
+%DOT_APP% -Tsvg -o%DOT_GV%.svg %DOT_GV%.gv
